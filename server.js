eddf28135c8db0a896279dd58d0a635a745bd804
Otmane Aatik <aaotmane@gmail.com>
2023-04-06 10:47:17 -0400

    Practice

--- server.js
+++ server.js
@@ -9,12 +9,21 @@ const app = express();
 //! Mount Middleware (app.use)
 
 //! Define routes
+//root or main route
 app.get('/', function(req,res){
     //use response object to send back data
-    res.send("<h1>Hello World!</h1>")
+    res.send("<h1>Hello Express!</h1>")
+})
+//home route
+app.get('/home', (req, res) => {
+    res.send("<h1>Home Page</h1>")
+})
+// another route
+app.get('/cars', (req,res) => {
+    res.json({cars:['honda', 'bmw']})
 })
 
 //! listen for requests 
 app.listen(3000, function(){
-    console.log('Server is running and listening on port 4000')
+    console.log('Server is running and listening on port 3000')
 })
